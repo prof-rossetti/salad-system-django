a34cad55913e13d007479e6c518326c0c265fad4
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2015-12-02 14:01:16 -0500

    rendering menu view from a template

--- menu_app/views.py
+++ menu_app/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("This is a Menu.")
+    template = loader.get_template('menu_items/index.html')
+    context = RequestContext(request, {
+        'menu_item_list': [1,2,3,4],
+    })
+    return HttpResponse(template.render(context))
